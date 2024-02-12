1ae07f0c975f60630fa5c82032755fcf6ab999ec
potof <slptmac@gmail.com>
2024-02-12 23:13:07 +0900

    update: refactoring

--- monitor-co2.py
+++ monitor-co2.py
@@ -7,12 +7,10 @@ from linebot.exceptions import LineBotApiError
 # CSV ファイルの場所を必要に応じて変える
 path = '/home/pi/logs/co2.csv'
 
-# 状態保存用のファイル
-state_file_path = 'previous_state.txt'
-
 # LINE チャネルアクセストークン
 CHANNEL_ACCESS_TOKEN = ''
 
+# LINE 経由で友達にメッセージを送る
 def send_message(text):
     line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
@@ -21,43 +19,65 @@ def send_message(text):
     except LineBotApiError as e:
         print(e.message)
 
-# 状態保存ファイルから前回の状態を読み込む
-try:
-    with open(state_file_path, 'r') as file:
-        previous_state = file.read().strip()
-except FileNotFoundError:
-    previous_state = None
+# 前回実行時の状態を取得する
+def get_previous_state(state_file_path: str):
+    try:
+        with open(state_file_path, 'r') as file:
+            return file.read().strip()
+    except FileNotFoundError:
+        return None
 
-# CSVファイルからデータを読み込む
-df = pd.read_csv(path, parse_dates=['timestamp'])
+# 状態をファイルに保存する
+def write_state(state: str, state_file_path: str):
+    with open(state_file_path, 'w') as file:
+        file.write(state)
 
-# 現在の時間を取得
-current_time = datetime.now()
+# レベル判定
+def get_state(co2: float):
+    if co2 >= 1000:
+        return "危険"
+    elif 900 <= co2 < 1000:
+        return "警告"
+    else:
+        return "正常"
 
-# 直近5分のデータを抽出
-recent_data = df[(df['timestamp'] >= current_time - timedelta(minutes=5)) & (df['timestamp'] <= current_time)]
+def main():
 
-# CO2の平均を計算
-average_co2 = recent_data['co2'].mean()
+    # CSVファイルからデータを読み込む
+    df = pd.read_csv(path, parse_dates=['timestamp'])
 
-# CO2の異常判定
-current_state = None
-message = ""
-if average_co2 >= 1000:
-    current_state = "危険"
-    message = "ちょっと高めなので換気して！"
-elif 900 <= average_co2 < 1000:
-    current_state = "警告"
-    message = "まだ大丈夫だけど換気できたらして！"
-else:
-    current_state = "正常"
-    message = "正常だよ！大丈夫！"
+    # 現在の時間を取得
+    current_time = datetime.now()
 
-# 状態が前回と変化した場合にメッセージを出力
-if previous_state is None or current_state != previous_state:
-    alert = "[ " + current_state + " ] CO2 濃度: " + str(average_co2) + "\n\n" + message
-    send_message(alert)
+    # 直近5分のデータを抽出
+    recent_data = df[(df['timestamp'] >= current_time - timedelta(minutes=5)) & (df['timestamp'] <= current_time)]
 
-# 現在の状態を外部ファイルに書き込む
-with open(state_file_path, 'w') as file:
-    file.write(current_state)
+    # CO2の平均を計算
+    average_co2 = recent_data['co2'].mean()
+
+    # CO2の異常判定
+    current_state = get_state(average_co2)
+
+    # 状態保存用のファイル
+    state_file_path = 'previous_state.txt'
+    previous_state = get_previous_state(state_file_path)
+
+    # 状態が前回と変化した場合にメッセージを出力
+    if previous_state is None or current_state != previous_state:
+
+        message = ""
+        if current_state == "危険":
+            message = "ちょっと高めなので換気して！"
+        elif current_state == "警告":
+            message = "まだ大丈夫だけど換気できたらして！"
+        elif current_state == "正常":
+            message = "正常だよ！大丈夫！"
+
+        alert = "[ " + current_state + " ] CO2 濃度: " + str(average_co2) + "\n\n" + message
+        send_message(alert)
+
+    write_state(current_state, state_file_path)
+
+
+if __name__ == "__main__":
+    main()
